6ba834589860885452b41b52ec16432e341b02ab
fyonecon <fyonecon@gmail.com>
2025-05-23 02:02:06 +0800

    1

--- view/pages/info/info.js
+++ view/pages/info/info.js
@@ -9,30 +9,6 @@ function show_info(){
             "value_class": "font-class",
         },
         {
-            "title": "浏览器主题",
-            "title_class": "font-text",
-            "value": view.scheme_model(),
-            "value_class": "font-text",
-        },
-        {
-            "title": "当前访问地址",
-            "title_class": "font-text",
-            "value": window.location.href,
-            "value_class": "font-text",
-        },
-        {
-            "title": "当前地址的Refer",
-            "title_class": "font-text",
-            "value": document.referrer,
-            "value_class": "font-text",
-        },
-        {
-            "title": "当前地址的Host",
-            "title_class": "font-text",
-            "value": window.location.host,
-            "value_class": "font-text",
-        },
-        {
             "title": "浏览器语言",
             "title_class": "font-text",
             "value": window.navigator.languages,
@@ -41,31 +17,25 @@ function show_info(){
         {
             "title": "浏览器时区",
             "title_class": "font-text",
-            "value": Intl.DateTimeFormat().resolvedOptions().timeZone || "不支持",
+            "value": Intl.DateTimeFormat().resolvedOptions().timeZone || "-不支持-",
             "value_class": "font-text",
         },
         {
             "title": "浏览器在线",
             "title_class": "font-text",
-            "value": window.navigator.onLine || "不支持",
+            "value": window.navigator.onLine || "-不支持-",
             "value_class": "font-text",
         },
         {
             "title": "设备CPU数(核)",
             "title_class": "font-text",
-            "value": window.navigator.hardwareConcurrency || "不支持",
+            "value": window.navigator.hardwareConcurrency || "-不支持-",
             "value_class": "font-text",
         },
         {
             "title": "设备内存(GB)",
             "title_class": "font-text",
-            "value": window.navigator.deviceMemory || "不支持",
-            "value_class": "font-text",
-        },
-        {
-            "title": "View框架解析用时",
-            "title_class": "font-text",
-            "value": (time_loaded - time_start) + " ms",
+            "value": window.navigator.deviceMemory || "-不支持-",
             "value_class": "font-text",
         },
         {
@@ -105,15 +75,57 @@ function show_info(){
             "value_class": "font-text",
         },
         {
-            "title": "APP UID",
+            "title": "IsMobileScreen",
             "title_class": "font-text",
-            "value": view.get_data(app_class + "app_uid"),
+            "value": view.is_mobile_screen(),
             "value_class": "font-text",
         },
         {
-            "title": "API URL",
+            "title": "IsUserScreen",
+            "title_class": "font-text",
+            "value": view.is_user_screen(),
+            "value_class": "font-text",
+        },
+        {
+            "title": "浏览器主题",
+            "title_class": "font-text",
+            "value": view.scheme_model(),
+            "value_class": "font-text",
+        },
+        {
+            "title": "View框架解析用时",
+            "title_class": "font-text",
+            "value": (time_loaded - time_start) + " ms",
+            "value_class": "font-text",
+        },
+        {
+            "title": "当前访问地址",
+            "title_class": "font-text",
+            "value": window.location.href,
+            "value_class": "font-text",
+        },
+        {
+            "title": "当前地址的Refer",
+            "title_class": "font-text",
+            "value": document.referrer,
+            "value_class": "font-text",
+        },
+        {
+            "title": "当前地址的Host",
+            "title_class": "font-text",
+            "value": window.location.host,
+            "value_class": "font-text",
+        },
+        {
+            "title": "主API地址",
             "title_class": "font-text",
             "value": api_url,
+            "value_class": "font-text",
+        },
+        {
+            "title": "APP UID",
+            "title_class": "font-text",
+            "value": view.get_data(app_class + "app_uid"),
             "value_class": "font-text",
         },
         {
@@ -141,18 +153,6 @@ function show_info(){
             "value_class": "font-text",
         },
         {
-            "title": "IsMobileScreen",
-            "title_class": "font-text",
-            "value": view.is_mobile_screen(),
-            "value_class": "font-text",
-        },
-        {
-            "title": "IsUserScreen",
-            "title_class": "font-text",
-            "value": view.is_user_screen(),
-            "value_class": "font-text",
-        },
-        {
             "title": "CDN加速",
             "title_class": "font-text",
             "value": [cdn_page_file, cdn_depend_file],
@@ -168,7 +168,7 @@ function show_info(){
 
     //
     test_fetch(window.location.href);
-    test_fetch(cdn_page_file + "pages/home/home.css?cache=" + files_version);
+    // test_fetch(cdn_page_file + "pages/home/home.css?cache=" + files_version);
 
 }
 
