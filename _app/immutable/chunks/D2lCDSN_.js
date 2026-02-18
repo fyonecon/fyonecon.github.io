19c102763931fb82c51ddb2b619c455919873715
fyonecon <fyonecon@users.noreply.github.com>
2026-02-18 11:42:10 +0000

    deploy: 67e0a2b8b5e31c46ce8df8a2737777a2c3fbd73b

---
+++ _app/immutable/chunks/D2lCDSN_.js
@@ -0,0 +1,1 @@
+import{s as r,p as e}from"./CZGZk7gc.js";import{ab as t}from"./DoShN9jh.js";const a={get error(){return e.error},get route(){return e.route},get status(){return e.status},get url(){return e.url}};r.updated.check;const u=a;t();export{u as p};
